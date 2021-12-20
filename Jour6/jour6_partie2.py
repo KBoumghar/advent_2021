618f9cb95ece4c20e1ca1ff314e3806b2eaa26b5
KBoumghar <karim.boumghar3@gmail.com>
2021-12-20 17:43:18 -0500

    jour 6 done easy

---
+++ Jour6/jour6_partie2.py
@@ -0,0 +1,36 @@
+fishList = []
+dayLimit = 8
+numberOfDay = 256
+listOfEight = [8]
+with open("input.txt") as f:
+    fishList = f.readline().split(",")
+
+
+listCounter = [0] * (dayLimit+1)
+for i in range (len(fishList)):
+    if fishList[i] == "1":
+        listCounter[1] += 1
+    elif fishList[i] == "2":
+        listCounter[2] += 1
+    elif fishList[i] == "3":
+        listCounter[3] += 1
+    elif fishList[i] == "4":
+        listCounter[4] += 1
+    elif fishList[i] == "5":
+        listCounter[5] += 1
+
+
+for i in range (numberOfDay):
+    temp = listCounter[0]
+    for j in range(len(listCounter)-1):
+        listCounter[j] = listCounter[j+1]
+    listCounter[6] += temp
+    listCounter[8] = temp
+
+
+counter = 0
+
+for i in range(len(listCounter)):
+    counter += listCounter[i]
+
+print(counter)
