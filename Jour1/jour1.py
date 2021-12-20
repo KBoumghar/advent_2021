2f261acc37907fdb868f96ac6714b226a13f4236
KBoumghar <karim.boumghar3@gmail.com>
2021-12-20 10:53:34 -0500

    test

---
+++ Jour1/jour1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+previous = float('inf')
+counter = 0
+numberList2 = ["1","2","3"]
+numberList = []
+with open("input.txt") as f:
+    numberList = [line.rstrip() for line in f]
+
+for x in numberList:
+    if int(x) > previous:
+        counter += 1
+    previous = int(x)
+
+print(counter)
